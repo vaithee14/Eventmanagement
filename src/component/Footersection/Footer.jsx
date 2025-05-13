6e42d2b70940fca4f921b8b8ce10227b7e51cb6a
vaithee14 <svaithi2004@gmail.com>
2025-05-13 17:02:08 +0530

    updates

--- src/component/Footersection/Footer.jsx
+++ src/component/Footersection/Footer.jsx
@@ -1,43 +1,17 @@
-import "./Footer.css";
 import grid1 from "../../assets/grid1.jpg";
 import grid2 from "../../assets/grid2.jpg";
 import beachcelebration from "../../assets/birthday.jpg";
 import weekend from "../../assets/music.jpg";
-import { useState } from "react";
-import axios from "axios";
+
+import "./Footer.css";
 
 export default function FooterSection() {
-  const [userEmail, setUserEmail] = useState("");
-
-  const addUserRegistration = async () => {
-    try {
-      const emailReg = { email };
-      const response = await axios.post(
-        "http://localhost:4050/user/subscrib/user/email",
-        emailReg,
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      alert(response.data.message);
-    } catch (error) {
-      console.error(error.response?.data?.message || error.message);
-    }
-  };
-
   return (
-    <section className="footer" id="service" >
+    <section className="footer" id="service">
       <div className="footer-left">
-        <h1 className="footer-logo">CRAZY EVENTS</h1>
+        <h1 className="footer-logo">EVENT PLANNER</h1>
         <p className="footer-description">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Necessitatibus laboriosam numquam deserunt voluptatibus vel ullam ad
-          reprehenderit sunt sint, nisi, eligendi earum quo reiciendis
-          architecto quas obcaecati accusantium nihil quis corrupti sequi eos
-          commodi. Vitae, adipisci. Fugit animi ipsum inventore autem, vel illum
-          consectetur ipsam excepturi error et minima commodi!
+          Subscribe to get latest updates about events, festivals, and more.
         </p>
       </div>
       <div className="footer-center">
